83050b6f05d2508baa65bc815ac8c0db38b772d9
Bart van Nierop <mail@bvnierop.nl>
2021-12-02 17:10:14 +0100

    Significantly improve 2021, Day 01

--- 2021/01.rkt
+++ 2021/01.rkt
@@ -2,19 +2,32 @@
 
 (require advent-of-code/aoc-lib)
 
+; Key insight:
+;  For both part one and two we only have to compare two numbers.
+;  For the example input: 199 200 208 210 200 207 240 269 260 263
+;  If the window is of size 3, the first two windows are
+;    [199 200 208]
+;        [200 208 210]
+;  Only 199/210 are different, and thus only these two have to be
+;  compared.
+;  So, for part one we have to test (< depths[n] depths[n-1])
+;  And for part two we have to test (< depths[n] depths[n-3])
+
+(: solve (-> (Listof String) Integer Integer))
+(define (solve lines distance)
+  (define depths (map string->int! lines))
+  (for/fold ([count 0])
+            ([d1 depths]
+             [d2 (drop depths distance)])
+    (if (< d1 d2) (+ count 1) (+ count))))
+  
 (: solve-a (-> (Listof String) Integer))
 (define (solve-a lines)
-  (define depths (map string->int! lines))
-  (define pairs (chunk-every 2 depths 1))
-  (count (lambda ((p : (Listof Integer))) (< (first p) (second p))) pairs))
+  (solve lines 1))
 
 (: solve-b (-> (Listof String) Integer))
 (define (solve-b lines)
-  (define depths (map string->int! lines))
-  (define windows : (Listof (Listof Integer)) (chunk-every 3 depths 1))
-  (define sums (map (lambda ((w : (Listof Integer))) (apply + w)) windows))
-  (define pairs (chunk-every 2 sums 1))
-  (count (lambda ((p : (Listof Integer))) (< (first p) (second p))) pairs))
+  (solve lines 3))
 
 (provide solve-a)
 (provide solve-b)
