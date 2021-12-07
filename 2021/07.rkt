baba0e2776c3f9933a2b0e0943cda1230f7deac7
Bart van Nierop <mail@bvnierop.nl>
2021-12-07 07:08:10 +0100

    Convert 2021, day 7 to typed/racket

--- 2021/07.rkt
+++ 2021/07.rkt
@@ -1,30 +1,30 @@
-#lang racket
-;;#lang typed/racket
+#lang typed/racket
 
 (require advent-of-code/aoc-lib)
 (require threading)
 
+(: parse (-> (Listof String) (Listof Integer)))
 (define (parse lines)
   (map string->int! (string-split (first lines) ",")))
 
-(define (solve-for positions real-cost-fn)
+(define (solve-for (positions : (Listof Integer)) (real-cost-fn : (-> Integer Integer)))
   (define start (first (sort positions <)))
   (define end (first (sort positions >)))
-  (for/fold ([fuel (foldl + 0 (map real-cost-fn positions))])
+  (for/fold ([fuel : Integer (foldl + 0 (map real-cost-fn positions))])
             ([i (in-range start (+ end 1))])
     (min fuel
-         (~> (map (λ (n) (abs (- n i))) positions)
+         (~> (map (λ ((n : Integer)) (abs (- n i))) positions)
              (map real-cost-fn _)
              (foldl + 0 _)))))
 
-;;(: solve-a (-> (Listof String) Integer))
+(: solve-a (-> (Listof String) Integer))
 (define (solve-a lines)
   (solve-for (parse lines) identity))
 
-;;(: solve-b (-> (Listof String) Integer))
+(: solve-b (-> (Listof String) Integer))
 (define (solve-b lines)
   (solve-for (parse lines)
-             (λ (n) (/ (* n (+ n 1)) 2))))
+             (λ ((n : Integer)) (exact-round (/ (* n (+ n 1)) 2)))))
 
 (provide solve-a)
 (provide solve-b)
