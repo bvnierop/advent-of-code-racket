b438ada6084bc79be5cc1e1876b707a119ff7cee
Bart van Nierop <mail@bvnierop.nl>
2021-12-17 19:57:36 +0100

    Convert 2021, day 14 to typed/racket

--- 2021/14.rkt
+++ 2021/14.rkt
@@ -1,17 +1,23 @@
-#lang racket
-;;#lang typed/racket
+#lang typed/racket
 
 (require advent-of-code/aoc-lib)
 (require threading)
 
+(define-type Transformations (Immutable-HashTable String (Listof String)))
+(define-type Counter (All (A) (HashTable A Integer)))
+
+(: make-transformations (-> (Listof (Listof String)) Transformations))
 (define (make-transformations transformations)
-  (foldl (λ (transformation lookup)
+  (define mkhash (inst hash String (Listof String)))
+  (define hashset (inst hash-set String (Listof String)))
+  (foldl (λ ((transformation : (Listof String)) (lookup : Transformations))
            (match transformation
              [(list from a b)
-              (hash-set lookup from (list a b))]))
-         (hash)
+              (hashset lookup from (list a b))]))
+         (mkhash)
          transformations))
 
+(: parse-line (-> String (Listof String)))
 (define (parse-line line)
   (match (string-split line " -> ")
     [(list pair insertion)
@@ -19,11 +25,16 @@
            (string-append (substring pair 0 1) insertion)
            (string-append insertion (substring pair 1 2)))]))
 
+(: make-pair-counter (-> (Listof String) (Counter String)))
 (define (make-pair-counter pairs)
-  (foldl (λ (pair counter) (hash-update counter pair (λ (v) (+ v 1)) (λ () 0)))
-         (hash)
+  (define mkhash (inst hash String Integer))
+  (define hupdate (inst hash-update String Integer))
+  (foldl (λ ((pair : String) (counter : (Counter String)))
+           (hupdate counter pair (λ (v) (+ v 1)) (λ () 0)))
+         (mkhash)
          pairs))
 
+(: parse-template (-> String (Counter String)))
 (define (parse-template line)
   (make-pair-counter
    (for/list ([a (in-string line)]
@@ -31,25 +42,30 @@
     (list->string (list a b)))))
 
 
+(: parse (-> (Listof String) (Pairof (Counter String) Transformations)))
 (define (parse lines)
   (let ([template (parse-template (first (take lines 1)))]
         [transformations (make-transformations (map parse-line (drop lines 2)))])
     (cons template transformations)))
 
+(: step (-> (Counter String) Transformations (Counter String)))
 (define (step pairs transformations)
-  (foldl (λ (pair new-pairs)
+  (define hupdate (inst hash-update String Integer))
+  (define mkhash (inst hash String Integer))
+  (foldl (λ ((pair : (Pairof String Integer)) (new-pairs : (Counter String)))
            (match pair
              [(cons from cnt)
               (~>> (hash-ref transformations from) ;; list: "BB" "BC" (ex)
-                  (foldl (λ (new-pair new-pairs)
-                           (hash-update new-pairs
-                                        new-pair
-                                        (λ (n) (+ n cnt))
-                                        (λ () 0)))
+                  (foldl (λ ((new-pair : String) (new-pairs : (Counter String)))
+                           (hupdate new-pairs
+                                    new-pair
+                                    (λ (n) (+ n cnt))
+                                    (λ () 0)))
                          new-pairs))]))
-         (hash)
+         (mkhash)
          (hash->list pairs)))
 
+(: step-many (-> (Counter String) Transformations Integer (Counter String)))
 (define (step-many pairs transformations n)
   (if (zero? n)
       pairs
@@ -58,28 +74,35 @@
        transformations
        (- n 1))))
 
+(: pairs->elements (-> (Counter String) (Counter Char)))
 (define (pairs->elements pairs)
+  (define hupdate (inst hash-update Char Integer))
+  (define mkhash (inst hash Char Integer))
   (~>> (hash->list pairs)
-       (foldl (λ (pair counter)
+       (foldl (λ ((pair : (Pairof String Integer)) (counter : (Counter Char)))
                 (match pair
                   [(cons from cnt)
                    (~>> (string->list from)
-                        (foldl (λ (elem counter)
-                                 (hash-update counter
-                                              elem
-                                              (λ (n) (+ n cnt))
-                                              (λ () 0)))
+                        (foldl (λ ((elem : Char) (counter : (Counter Char)))
+                                 (hupdate counter
+                                          elem
+                                          (λ (n) (+ n cnt))
+                                          (λ () 0)))
                                counter))]))
-              (hash))))
+              (mkhash))))
 
+(: normalize (-> Integer Integer))
 (define (normalize value)
   (exact-ceiling (/ value 2)))
 
+(: score (-> (Counter Char) Integer))
 (define (score elements)
-  (let ([sorted-values (sort (hash-values elements) <)])
+  (define isort (inst sort Integer))
+  (let ([sorted-values (isort (hash-values elements) <)])
     (- (normalize (first (reverse sorted-values)))
        (normalize (first sorted-values)))))
 
+(: solve (-> (Listof String) Integer Integer))
 (define (solve lines n)
   (match (parse lines)
     [(cons pairs transformations)
@@ -88,11 +111,11 @@
          (score))]))
   
 
-;;(: solve-a (-> (Listof String) Integer))
+(: solve-a (-> (Listof String) Integer))
 (define (solve-a lines)
   (solve lines 10))
 
-;;(: solve-b (-> (Listof String) Integer))
+(: solve-b (-> (Listof String) Integer))
 (define (solve-b lines)
   (solve lines 40))
 
@@ -100,7 +123,7 @@
 (provide solve-b)
 
 (module+ test
-  (require rackunit)
+  (require typed/rackunit)
   (check-equal?
    (solve-a (file->lines "input/14test.rktd"))
    1588)
